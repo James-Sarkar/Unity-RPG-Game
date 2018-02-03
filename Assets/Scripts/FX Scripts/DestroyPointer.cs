55cfe535c64120a37980e4b069415bf866af1d56
James Sarkar <James.Sarkar@sap.com>
2018-02-03 15:18:19 -0500

    Some cleanup

---
+++ Assets/Scripts/FX Scripts/DestroyPointer.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyPointer : MonoBehaviour {
+
+	private Transform player;
+
+	void Start () {
+		player = GameObject.FindGameObjectWithTag ("Player").transform;
+	}
+
+	void Update () {
+		if (Vector3.Distance (transform.position, player.position) <= 1f) {
+			Destroy (gameObject);
+		}	
+	}
+}
