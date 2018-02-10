21fc4faa240602eff7936179eeaf31cba6df22de
James Sarkar <James.Sarkar@sap.com>
2018-02-10 14:54:27 -0500

    Added scripts for enemy movement and enemy health

---
+++ Assets/Scripts/Enemy Scripts/EnemyHealth.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EnemyHealth : MonoBehaviour {
+
+	public float health = 100f;
+
+	public Image healthImg;
+
+	public void TakeDamage(float amount) {
+		health -= amount;
+
+		healthImg.fillAmount = health / 100f;
+
+		if (health <= 0) {
+
+		}
+	}
+}
---
+++ Assets/Scripts/FX Scripts/SkillDamage.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkillDamage : MonoBehaviour {
+
+	public LayerMask enemyLayer;
+
+	public float radius = 0.5f, damageCount = 10f;
+
+	private EnemyHealth enemyHealth;
+
+	private bool collided;
+
+	void Update () {
+		Collider[] hits = Physics.OverlapSphere (transform.position, radius, enemyLayer);
+
+		foreach (Collider c in hits) {
+			enemyHealth = c.gameObject.GetComponent<EnemyHealth> ();
+
+			collided = true;
+		}
+
+		if (collided) {
+			enemyHealth.TakeDamage (damageCount);
+
+			enabled = false;
+		}
+	}
+}
